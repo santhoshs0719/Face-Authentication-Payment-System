bca02283ac390aab94ac702d9bee7bafb5d0f13f
santhoshs0719 <santhoshsundaram712@gmail.com>
2023-09-18 12:15:25 +0530

    Configuration completed.

--- Online Face Based Authentication Payment System/home.py
+++ Online Face Based Authentication Payment System/home.py
@@ -34,7 +34,7 @@ def admin():
 @app.route("/admin_login")
 def admin1():
     return render_template('admin.html')
-############################################Test#####################################################################3
+#################################################################################################################3
 @app.route("/student_deposite")
 def student_deposite():
     un=session['uname']
